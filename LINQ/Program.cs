a01b67cdc05a69a98ee60c895d0c9cc5ae648956
skyler dennis <skylerdenniss11@gmail.com>
2025-03-18 10:20:58 -0400

    I finnished creating a ling of video game names ordered by length.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> VideoGames = new List<string>();
+            VideoGames.Add("* Rainbow Six Siege,");
+            VideoGames.Add("* For Honor,");
+            VideoGames.Add("* Apex Legends,");
+            VideoGames.Add("* Fall Guys.");
+
+            var OrderedGameNames = VideoGames.OrderByDescending(name => name.Length);
+
+            Console.WriteLine("Video Game Names Listed By Length!");
+            Console.WriteLine("----------------------------------");
+            foreach(var VideoGame in OrderedGameNames)
+            {
+                Console.WriteLine(VideoGame);
+            }
+
+
+                
         }
     }
 }
